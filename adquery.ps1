7c96c35bae97eee522859ff3a121485b6e7cd543
Moe <57568144+MoeChad@users.noreply.github.com>
2020-06-14 21:29:51 -0400

    Add files via upload

---
+++ adquery.ps1
@@ -0,0 +1,23 @@
+﻿function Get-LogonWorkstation {
+    [cmdletbinding()]
+    param(
+        [parameter(Mandatory)]
+        [string]$Username
+    )
+
+    try {
+        $Account = Get-ADUser -Identity $Username -Properties LogonWorkstations | Select-Object -ExpandProperty LogonWorkstations
+        Write-Verbose -Message ('Located User {0}' -f $Username)
+    }
+    catch {
+        Throw('Unable to locate user {0}' -f $Username)
+    }
+
+    $Account -split ',' | ForEach-Object {
+    $Computer = Get-ADComputer -Identity $_ -ErrorAction SilentlyContinue | Select-Object -ExpandProperty Enabled
+        [PSCustomObject]@{
+            ComputerName = $_
+            Enabled = [bool]$Computer.Enabled
+            }
+        } | Export-csv -Path data.csv
+}
